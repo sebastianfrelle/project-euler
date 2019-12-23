a9de94b78a5c257a063cb9ed8c09b36ff78aba7e
Sebastian Frelle Koch <sebastian.frelle@gmail.com>
2019-12-23 16:08:23 +0100

    problem_4

--- elixir/project_euler.ex
+++ elixir/project_euler.ex
@@ -16,4 +16,46 @@ defmodule ProjectEuler do
     # Calculate the sum
     |> Enum.sum()
   end
+
+  def p3 do
+    # TODO
+  end
+
+  def p4 do
+    # Let's start by writing a stream that produces all 899 (999 - 100)
+    # palindromes.
+
+    # The product of two 3-digit numbers can never be more than 999_999.
+    # It can never be less than 10_000. Since we're working with palindromes,
+    # it can never be less than 10_001.
+
+    # I need something like this:
+    # 999_999, 998_899, 997_799, ..., 899_998, 898_898, 897_798, ..., 100_001,
+    # 99_999, 99899, 99799, 99699, ..., 98989, 98889, 98789, 98689, ...
+    # palindromes = Stream.iterate(999_999, fn x -> x - 1_100 end)
+    palindromes_six_digits =
+        for i <- 0..8, j <- 0..9, k <- 0..9 do
+          999_999 - 100_001 * i - 10_010 * j - 1_100 * k
+        end
+
+      palindromes_five_digits =
+        for i <- 0..8, j <- 0..9, k <- 0..9 do
+          99_999 - 10_001 * i - 1_010 * j - 100 * k
+        end
+
+      palindromes_six_digits
+      |> Stream.concat(palindromes_five_digits)
+      |> Enum.find(fn x ->
+        Enum.any?(999..100, fn y -> rem(x, y) == 0 and x / y < 1000 end)
+      end)
+    end
+  end
+
+  def benchmark(f) do
+    f
+    |> :timer.tc()
+    |> elem(0)
+    |> Kernel./(1_000_000)
+    |> Kernel./(5000
+  end
 end
